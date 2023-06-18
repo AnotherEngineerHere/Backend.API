28fba79b47646dff2be7a22bed45e3a1674d83a4
Andres Orozco <juanxxi2015@gmail.com>
2023-06-18 16:06:44 -0500

    Added frontend
    Added frontend and changed the running port

--- Backend.API/Program.cs
+++ Backend.API/Program.cs
@@ -28,11 +28,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
