057e5a4ba3122c6a9cc981510d1fdc5e7929b7b0
Juan Andres Orozco <juanxxi2015@gmail.com>
2023-06-20 12:04:43 -0500

    Frontend to show user in progress change the mockup data to the get one

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,21 +1,41 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ViewChild, OnInit} from '@angular/core';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-
+import { Cliente } from './Interfaces/cliente';
+import {ClienteService} from './Services/cliente.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements AfterViewInit, OnInit{
   title = 'FrontEnd';
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  displayedColumns: string[] = ['nombre', 'apellido', 'email', 'fechaDeNacimiento', 'activo', 'rol', 'direccion', 'telefono', 'Acciones'];
+  dataSource = new MatTableDataSource<Cliente>();
+  constructor(private _clienteService: ClienteService){
+
+  }
+  ngOnInit(): void {
+      
+  }
+
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  showClientes(){
+    this._clienteService.getList().subscribe({
+      next:(dataResponse) => {
+        console.log(dataResponse);
+      }
+    })
   }
 }
 export interface PeriodicElement {
