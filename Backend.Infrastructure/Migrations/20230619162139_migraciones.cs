d88816ce4aa71d3c50a5368efc15f7312df1e4f7
Andres Orozco <juanxxi2015@gmail.com>
2023-06-20 20:02:52 -0500

    Commit from home laptop

---
+++ Backend.Infrastructure/Migrations/20230619162139_migraciones.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Infrastructure.Migrations
+{
+    public partial class migraciones : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    first_name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    last_name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    rol = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    date_of_birth = table.Column<DateTime>(type: "date", nullable: false),
+                    is_active = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false),
+                    address = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    phone_number = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Clientes_Usuarios_id",
+                        column: x => x.id,
+                        principalTable: "Usuarios",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Empleados",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleados", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Empleados_Usuarios_id",
+                        column: x => x.id,
+                        principalTable: "Usuarios",
+                        principalColumn: "id");
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Empleados");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
